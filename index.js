87788541e3b20cd1c617c7b1000a2195772268bf
tranha18nd <tranha18nd@gmail.com>
2020-07-27 11:09:44 +0700

    add app chat

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require("express")
+var app = express()
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+app.set("views", "./views")
+
+var server = require("http").Server(app)
+var io = require("socket.io")(server)
+server.listen(3000)
+var i = 0
+var arrUsers = []
+io.on("connection", function(socket){
+	console.log("Hello! wellcome you connect to server", socket.id)
+	i++
+	console.log(i, "client connected")
+
+	socket.on("client send userName", function(data){
+		if(arrUsers.indexOf(data) >=0){
+			socket.emit("server send err", "Đăng ký thất bại. Vui lòng kiểm tra lại")
+		} else {
+			arrUsers.push(data)
+			socket.userName = data
+			socket.emit("server send success", data)
+			io.sockets.emit("server send new user", arrUsers)
+		}
+	})
+
+	socket.on("client logout", function(){
+		// console.log(socket.userName)
+		arrUsers.splice(arrUsers.indexOf(socket.userName), 1)
+		// console.log(arrUsers)
+		socket.broadcast.emit("server send new user", arrUsers)
+	})
+
+
+	socket.on("client send message", function(data){
+		io.sockets.emit("server send message", {user:socket.userName, content:data})
+	})
+	
+
+	socket.on("client input", function(){
+		io.sockets.emit("server send notice", socket.userName)
+	})
+
+	socket.on("client output", function(){
+		io.sockets.emit("server send unotice", socket.userName)
+	})
+})
+
+app.get("/", function(req, res){
+    res.render("home")
+})
---
+++ public/myQuery.js
@@ -0,0 +1,74 @@
+    $('#loginForm').show()
+    $('#chatForm').hide()
+
+    
+
+
+
+$(document).ready(function(){
+	var socket = io("http://172.26.11.50:3000")
+	$('#btnRegister').click(function(){
+		var	userName = $('#userName').val()	
+		socket.emit("client send userName", userName)
+	})
+
+	socket.on("server send err", function(data){
+		alert(data)
+	})
+
+	socket.on("server send success", function(data){
+		alert("Đăng ký thành công")
+		$("#currentUser").html(data)
+
+		$('#loginForm').hide(2000)
+    	$('#chatForm').show(1000)
+	})
+	
+	socket.on("server send new user", function(data){
+		$("#boxContent").html('')
+		data.forEach(function(i){
+			$("#boxContent").append("<div class = 'user'>"+i+"</div>")
+		})
+	})
+
+	socket.on("server send message", function(data){
+		$("#listMessages").append("<div class = 'message'><b>"+data.user +":</b>"+data.content+"</div>")
+	})
+
+	socket.on("server send notice", function(data){
+		$("#notice").html("<img src = 'typing.gif' style = 'width:30px;'><span>"+data+" đang nhập tin nhắn....</span>")
+	})
+
+	socket.on("server send unotice", function(data){
+		$("#notice").html("")
+	})
+
+	$('#btnLogout').click(function(){
+		socket.emit("client logout")
+		$('#loginForm').show(2000)
+    	$('#chatForm').hide(1000)
+	})
+
+	$('#sendMessage').click(function(){
+		var	txtMessage = $('#txtMessage').val()	
+		socket.emit("client send message", txtMessage)
+		$('#txtMessage').val('')
+		$('#txtMessage').focus()
+	})
+	$("#txtMessage").keyup(function(event) {
+    	if (event.keyCode === 13) {
+			var	txtMessage = $('#txtMessage').val()	
+			socket.emit("client send message", txtMessage)
+			$('#txtMessage').val('')
+			$('#txtMessage').focus()
+		}
+	})
+
+	$('#txtMessage').focusin(function(){
+		socket.emit("client input")
+	})
+
+	$('#txtMessage').focusout(function(){
+		socket.emit("client output")
+	})
+})
